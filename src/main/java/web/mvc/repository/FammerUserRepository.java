da8dbb436b02ccb3e46148dd805f477e22916eb9
kimjaeyou <unknown88455144@gmail.com>
2024-11-18 14:02:15 +0900

    24/11/18/14:01-로긍인 기능 -재영

--- src/main/java/web/mvc/repository/FammerUserRepository.java
+++
@@ -1,9 +0,0 @@
-package web.mvc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import web.mvc.domain.FarmerUser;
-import web.mvc.domain.ManagementUser;
-
-public interface FammerUserRepository extends JpaRepository<FarmerUser, Long> {
-}
