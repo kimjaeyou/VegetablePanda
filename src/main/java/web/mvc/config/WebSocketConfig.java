7d4438786f314363dd2ce45a26aedbf4179a8b1d
kimjaeyou <unknown88455144@gmail.com>
2024-11-20 18:57:18 +0900

    commit

--- src/main/java/web/mvc/config/WebSocketConfig.java
+++ src/main/java/web/mvc/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         System.out.println(" registerStompEndpoints(StompEndpointRegistry registry...");
         registry.addEndpoint("/ws") // WebSocket 엔드포인트
                 .setAllowedOrigins("http://localhost:5173"); // React 앱 주소
-                //.setAllowedOrigins("*");
-                //.withSockJS(); // SockJS를 통한 WebSocket fallback 지원
+        //.setAllowedOrigins("*");
+        //.withSockJS(); // SockJS를 통한 WebSocket fallback 지원
     }
 }
