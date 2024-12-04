05915d1b2c8e4d4eefbe38a50f425ddfc5344135
kim <sidmian@gmail.com>
2024-12-04 10:09:52 +0900

    24-12-04 10:07 reviewComment 수정, reviewCommentDTO 수정, userByDetailㄲepository에 쿼리문 추가 ManagementRepository에 메소드 추가. -성민

--- src/main/java/web/mvc/service/ReviewCommentService.java
+++ src/main/java/web/mvc/service/ReviewCommentService.java
@@ -9,10 +9,10 @@ import java.util.List;
 public interface ReviewCommentService {
 
     // 댓글 등록
-    ReviewCommentDTO reviewCommentSave(Long reviewSeq, ReviewCommentDTO reviewCommentDTO, MultipartFile image);
+    ReviewCommentDTO reviewCommentSave(Long reviewSeq, ReviewCommentDTO reviewCommentDTO, MultipartFile file);
 
     // 댓글 수정
-    ReviewCommentDTO reviewCommentUpdate(Long reviewSeq, Long reviewCommentSeq, ReviewCommentDTO reviewCommentDTO, MultipartFile image, boolean deleteFile);
+    ReviewCommentDTO reviewCommentUpdate(Long reviewSeq, Long reviewCommentSeq, ReviewCommentDTO reviewCommentDTO, MultipartFile file, boolean deleteFile);
 
     // 특정 사용자가 작성한 댓글 조회
     List<ReviewCommentDTO> reviewCommentFindAllByUserId(Long userId);
